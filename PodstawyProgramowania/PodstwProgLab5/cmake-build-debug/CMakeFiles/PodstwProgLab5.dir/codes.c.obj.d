34893c951e8f002e6206f9ae29539a10138e98f1
Rastaxd <jakub.weiss03@gmail.com>
2023-11-17 15:01:34 +0100

    Update

--- PodstawyProgramowania/PodstwProgLab5/cmake-build-debug/CMakeFiles/PodstwProgLab5.dir/codes.c.obj.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/PodstwProgLab5.dir/codes.c.obj: \
- D:\Repositories\Studia\PodstawyProgramowania\PodstwProgLab5\codes.c \
- D:\Repositories\Studia\PodstawyProgramowania\PodstwProgLab5\codes.h
--- PodstawyProgramowania/PodstwProgLab5/cmake-build-debug/CMakeFiles/PodstwProgLab5.dir/codes.cpp.obj.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/PodstwProgLab5.dir/codes.cpp.obj: \
- D:\Repositories\Studia\PodstawyProgramowania\PodstwProgLab5\codes.cpp \
- D:\Repositories\Studia\PodstawyProgramowania\PodstwProgLab5\codes.h
