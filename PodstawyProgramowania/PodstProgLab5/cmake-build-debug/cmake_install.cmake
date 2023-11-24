f82afaf929c6483bd2b02320337eca59ae627f2d
Rastaxd <jakub.weiss03@gmail.com>
2023-11-24 12:26:32 +0100

    Update.

--- PodstawyProgramowania/PodstProgLab5/cmake-build-debug/cmake_install.cmake
+++ PodstawyProgramowania/PodstProgLab5/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Repositories/Studia/PodstawyProgramowania/PodstwProgLab5
+# Install script for directory: C:/Repositories/Studia/PodstawyProgramowania/PodstProgLab5
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Repositories/Studia/PodstawyProgramowania/PodstwProgLab5/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Repositories/Studia/PodstawyProgramowania/PodstProgLab5/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
